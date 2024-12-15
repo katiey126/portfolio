75307f2a79690066b4af1fc2c803917d6357c52a
Katie Yang <113875818+PrecambrianSoup@users.noreply.github.com>
2024-12-14 23:07:21 -0500

    project page layout change

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Name: Katie Yang
 Responsive page: about page
 
 I had the most trouble with the projects page since I had to figure out how to position
-images inside the frames and with things having different aspect ratios, but it eventually
-worked out as I had intended. I also changed the format of the about page just because I 
-realized I didn't want to use a photo.
+images inside the frames with things having different aspect ratios and the frame not being 
+perfectly rectangular, so I changed the design to have just the images without the frames.
+I also changed the format of the about page because I realized I didn't want to use a photo.
 
 Some changes I would like to make in the future:
 - Adding a customized hand drawn cursor
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,38 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 import project1 from "public/project1.png";
 import project2 from "public/project2.png";
+import person from "public/crayon-stick-figure.png";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="flex items-center flex-row text-2xl font-crayonWorks justify-center">
-
-        <div className="flex flex-col items-center">
-          <div className="flex flex-col relative w-6/6 h-auto flex items-center justify-center">
-            <Image
-              src={project1}
-              alt="bullet journal app"
-            />
+      <div className="flex items-center flex-row text-2xl font-crayonWorks justify-center gap-20">
+          <div className="flex flex-col relative w-4/12 h-auto flex items-center justify-center transform translate-y-14">
+            <Image src={person} alt="figure" className="w-12 h-auto translate-x-24"/>
+            <Image src={project1} alt="bullet journal app"/>
           
             <span className="px-8 text-center">
-              Bullet journal styled to-do list and scheduler with auto assignment import via syllabus parsing.
+              Bullet journal styled to-do list/scheduler with auto assignment import via syllabus parsing.
             </span>
           </div>
-        </div>
 
-        <div className="flex flex-col items-center">
-          <div className="flex flex-col relative w-4/6 h-auto flex items-center justify-center">
-            <Image
-              src={project2}
-              alt="fcover ml research"
-            />
+          <div className="flex flex-col relative w-5/12 h-auto flex items-center justify-center transform -translate-y-6">
+            <Image src={project2} alt="fcover ml research" />
           
             <span className="px-8 text-center">
-              developed machine learning model to predict vegetation cover from satellite reflectance data
+              Machine learning model for predicting vegetation cover from satellite reflectance data
             </span>
           </div>
-        </div>
-
       </div>
     </div>
   );
